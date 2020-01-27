d2375a99ccb807be1661b37698561bfba5ddad40
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-01-28 04:29:33 +0500

    Refactor

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import './RandomPlanet.css'
 import Loader from '../Loader'
@@ -9,22 +9,19 @@ export default class RandomPlanet extends Component {
     apiService: PropTypes.object.isRequired
   }
 
-  displayFields = [
-    'name', 'population', 'diameter', 'rotationPeriod', 'orbitalPeriod',
-    'climate', 'terrain', 'surfaceWater'
-  ]
-
   state = {
     isLoading: true,
-    id: null,
-    name: 'planet',
-    population: 0,
-    diameter: '',
-    rotationPeriod: '',
-    orbitalPeriod: '',
-    climate: '',
-    terrain: '',
-    surfaceWater: ''
+    planet: {
+      id: null,
+      name: 'planet',
+      population: 0,
+      diameter: '',
+      rotationPeriod: '',
+      orbitalPeriod: '',
+      climate: '',
+      terrain: '',
+      surfaceWater: ''
+    }
   }
 
   constructor(props) {
@@ -40,44 +37,54 @@ export default class RandomPlanet extends Component {
   async updateData() {
     const id = Math.floor(Math.random() * 10) + 1
     const planet = await this.apiService.getPlanet(id)
-    this.setState({ ...planet, isLoading: false })
+    this.setState({
+      planet,
+      isLoading: false
+    })
   }
 
   render() {
-    const { name, imgSrc, isLoading } = this.state
-
-    if (isLoading) {
-      return(<Loader />)
-    }
-
-    const elements = this.displayFields.map( key => (
-      <li
-        className="list-group-item d-flex justify-content-between align-items-center"
-        key={ key }
-      >
-        { key }
-        <span className="badge badge-primary badge-pill">{ this.state[key] }</span>
-      </li>
-    ))
+    const { isLoading, planet } = this.state
 
     return(
       <div className="random-planet card w-50">
-        <header className="card-header">
-          { name }
-        </header>
-        <div className="card-body d-flex justify-content-between">
-          <div className="planet-image w-25">
-            <img
-              src={ imgSrc }
-              className="img-fluid"
-              alt="Planet"
-            />
-          </div>
-          <ul className="list-group ml-3 flex-grow-1">
-            { elements }
-          </ul>
-        </div>
+        { isLoading ? <Loader /> : <ViewPlanet planet={planet} /> }
       </div>
     )
   }
+}
+
+
+function ViewPlanet({ planet }) {
+  const { id, imageSrc, name, ...displayProps } = planet
+
+  const elements = Object.keys(displayProps).map( key => (
+    <li
+      className="list-group-item d-flex justify-content-between align-items-center"
+      key={ key }
+    >
+      { key }
+      <span className="badge badge-primary">{ displayProps[key] }</span>
+    </li>
+  ))
+
+  return(
+    <Fragment>
+      <header className="card-header">
+        { name }
+      </header>
+      <div className="card-body d-flex justify-content-between">
+        <div className="planet-image w-25">
+          <img
+            src={ imageSrc }
+            className="img-fluid"
+            alt="Planet"
+          />
+        </div>
+        <ul className="list-group ml-3 flex-grow-1">
+          { elements }
+        </ul>
+      </div>
+    </Fragment>
+  )
 }
