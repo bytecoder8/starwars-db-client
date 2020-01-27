b582560de7ef9de84103804832dd2b4ffddd64c0
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-01-28 01:50:18 +0500

    Use dotenv, remove config

--- src/hocs/withApiService.js
+++ src/hocs/withApiService.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import config from '../config'
 import MockApiService from '../services/MockApiService'
 import SWApiService from '../services/SWApiService'
 
 function withApiService(WrappedComponent) {
   return class extends Component {
     render() {
-      const apiService = config.API_SERVICE === 'mock'
+      const apiService = process.env.REACT_APP_API === 'mock'
         ? new MockApiService() : new SWApiService()
 
       return(
