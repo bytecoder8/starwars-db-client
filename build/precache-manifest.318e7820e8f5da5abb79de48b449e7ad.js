24c62a9162da46bf42abbd7de0802445ab052448
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-03-27 13:03:42 +0500

    Add files via upload

---
+++ build/precache-manifest.318e7820e8f5da5abb79de48b449e7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa83fdbfad7d71392874b754eac98be5",
+    "url": "/starwars-db-client/build/index.html"
+  },
+  {
+    "revision": "69da0e96b688f395a237",
+    "url": "/starwars-db-client/build/static/css/main.349794c0.chunk.css"
+  },
+  {
+    "revision": "7ca3f0075b402e7a9f76",
+    "url": "/starwars-db-client/build/static/js/2.f3b8bf7c.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/starwars-db-client/build/static/js/2.f3b8bf7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69da0e96b688f395a237",
+    "url": "/starwars-db-client/build/static/js/main.19995c8b.chunk.js"
+  },
+  {
+    "revision": "7c8c55c124bbdd02669d",
+    "url": "/starwars-db-client/build/static/js/runtime-main.7ae400f8.js"
+  }
+]);
